303773e97b97cbb236ce7fbe5e4e98de95afac3c
MuhdMusab <musab653@gmail.com>
2022-05-24 18:25:04 +0800

    Updated algorithm

--- lib/helper/time_widget.dart
+++ lib/helper/time_widget.dart
@@ -23,7 +23,7 @@ class _TimeWidgetState extends State<TimeWidget> {
       child: Text(
         widget.time! > 1 ? widget.time.toString() + " mins" : widget.time.toString() + " min",
         style: TextStyle(
-          color: Colors.white,
+          color: Colors.white.withOpacity(widget.visibility == 0.0 ? 0.0 : 1.0),
           fontWeight: FontWeight.bold,
         ),
       ),
